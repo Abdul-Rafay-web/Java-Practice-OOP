2896298ad3cbdb98d89f5bbde954990698a7b254
Abdul Rafay Tariq <rafay.art.276@gmail.com>
2025-07-14 23:25:49 +0500

    Add files via upload

---
+++ Q10/Person.java
@@ -0,0 +1,10 @@
+package JavaPractice.Q10;
+
+public class Person {
+    protected String name;
+    protected int age;
+    Person(String name,int age){
+        this.age=age;
+        this.name= name;
+    }
+}
